81b125914441b375ce62ad2697d98ebd4cb62552
Even Lislebø <e.lislebo@gmail.com>
2015-02-13 12:45:55 +0100

    Moving arrays between host & device memories
    Issue #1 weird device memory bug

--- pymp/blur.py
+++ pymp/blur.py
@@ -7,20 +7,23 @@ def blur(input_img, blurred_img):
   index = cuda.grid(1)
   if(index >= input_img.shape[0]):
     return
-  blurred_img[index] = input_img[index] * 2
+  blurred_img[index] = 4.0
 
 
 if __name__ == "__main__":
-  img = np.ones(20)
-  blurred_img = np.zeros(20)
+  img = np.ones(100)
+  blurred_img = np.zeros(100)
   d_input_img = cuda.to_device(img)
   d_blurred_img = cuda.device_array(img.shape[0])
+
   threads_per_block = 256
   n_blocks = (img.shape[0] + threads_per_block-1) / threads_per_block
 
-  for num in img:
+  for num in blurred_img:
     print num
   blur[n_blocks,threads_per_block](d_input_img, d_blurred_img)
+
+  print("####################")
 
   d_blurred_img.copy_to_host(blurred_img)
   for num in blurred_img:
